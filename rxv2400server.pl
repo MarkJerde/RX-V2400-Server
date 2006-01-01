a520081620dc8e99c7fdde8b4942e8abdc45c368
Mark Jerde <mjerde3@charter.net>
2006-01-01 11:41:00 -0600

    Oldest version I could find laying around in backups.  No clue if it works.

--- rxv2400server.pl
+++ rxv2400server.pl
@@ -1,8 +1,43 @@
 #! perl -w
 
-# version 1.1 of linxServer.pl #####################################
+#   RX-V2400 Server
+#   Control software for certain Yamaha A/V receivers.
+#   This is independently developed software with no relation to Yamaha.
+#
+#   Copyright 2006 Mark Jerde
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+#   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+#   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+#   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+#   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+# version 0.1 of rxv2400server.pl ##################################
 ####################################################################
-############### hacked by jörg dießl, fall 99 (joreg@testphase.at) #
+############## hacked by Mark Jerde, fall 05 (mjerde3@charter.net) #
+####################################################################
+# based on the original version linxServer.pl 1.1 ##################
+# hacked by jörg dießl, fall 99 (joreg@testphase.at) ###############
 ####################################################################
 # based on the original version talkrcx.txt,v 1.3 ##################
 # written by Paul Haas (http://www.hamjudo.com/rcx/talkrcx.txt) ####
@@ -16,14 +51,11 @@
 # this server listens to port 9000 for the following commands:	   #
 # "vor", "stop", "zruck" and "view" 							   #
 # corresponding to the command a string is sent to the serial-port #
-# (to which a cybermaster should be connected) that starts one	   #
-# of the four tasks that may be stored in a cybermaster 		   #
+# (to which a Yamaha RX-V 2400 should be connected) that performs  #
+# the operations allowed by the Yamaha receiver         		   #
 ####################################################################
 # before the server starts listening...							   #
 # ...the serial-port is opened									   #
-# ...the firmware is unlocked									   #
-# ...variables 0 and 1 are set to 1 (starting values for viewchange [required in loop.nqc])
-# ...the power down delay is set to 0 (infinite)				   #
 ####################################################################
 ####################################################################
 # linx for more info on rcx-programming: ###########################
@@ -37,14 +69,40 @@ use IO::Socket;
 use Net::hostent;
 use Sys::Hostname;
 
+my %yamaha = ( "System" => "1", "Power" => "0" ); # Busy and Off
+
+my $STX = hex2str("02");
+my $ETX = hex2str("03");
+
+my %Spec =
+( "R0161" =>
+  { "Configuration" =>
+    { "System" => { "OK" => "0" , "Busy" => "1" },
+      "Power" => { "OFF" => "0" , "ON" => "1" }
+    },
+    "Control" =>
+    { "Prefix" => $STX,
+      "Suffix" => $ETX,
+      "Operation" =>
+      { "Prefix" => "07",
+        "Power" => { "Prefix" => "A1" , "ON" => "D" , "OFF" => "E" }
+      },
+      "System" =>
+      { "Prefix" => "2"
+      }
+    }
+  }
+);
+
 my $PortName = "COM1";
 my $PortObj = new Win32::SerialPort ($PortName)
        || die "Can't open $PortName: $^E\n";
 
 $PortObj->databits(8);
-$PortObj->baudrate(2400);
-$PortObj->parity("odd");
-$PortObj->parity_enable(1);
+$PortObj->baudrate(9600);
+$PortObj->parity("none");
+$PortObj->handshake("none");
+$PortObj->parity_enable(F);
 $PortObj->stopbits(1);
 $PortObj->buffers(4096, 4096);
 $PortObj->dtr_active(1);
@@ -55,13 +113,148 @@ if ($PortObj->write_settings != 1)
 	print "sorry, couldn't setup serial port";
 }
 
-print "unlocking firmware...\n";
-# "Do you byte, when I knock?"
-$unlockMSG = "FE	00	00	FF	A5	5A	44	BB	6F	90	20	DF	79	86	6F	90	75	8A	20	DF	62	9D	79	86	74	8B	65	9A	2C	D3	20	DF	77	88	68	97	65	9A	6E	91	20	DF	49	B6	20	DF	6B	94	6E	91	6F	 90	63	9C	6B	94	3F	C0	85	7A";
-$unlockMSG = hex2str($unlockMSG);
-$response = str2hex (getMsg($unlockMSG));
+sub sendInit
+{
+	print "Sending undocumented init...\n";
+	$PortObj->write("0000020100");
+	($count_in, $string_in) = $PortObj->read(275);
+	print "Got $count_in bytes back.\n";
+	$string_in = "";
+	print "\n";
+}
 
-sleep(1);
+sub sendReady # getConfiguration
+{
+	print "Sending Ready command...\n";
+	$PortObj->write("000");
+	($count_in, $response) = $PortObj->read(512);
+	print "Got $count_in bytes, reading Configuration...\n";
+
+	# DCn
+	$response =~ s/(.)//;
+	$_ = str2hex($1);
+	if ( m/11/ ) { print(" DC1\n"); }
+	elsif ( m/12/ ) { print(" DC2\n"); }
+	elsif ( m/13/ ) { print(" DC3\n"); }
+	elsif ( m/14/ ) { print(" DC4\n"); }
+	else { print "error: No DCn\n" and exit; }
+	
+	print " Received Configuration...\n";
+	# 5B Model ID
+	$yamaha{'ModelID'} = substr($response,0,5);
+	print "  model ID: ".$yamaha{'ModelID'};
+	if ( $yamaha{'ModelID'} eq "R0161" ) { print " (RX-V2400)"; }
+	print "\n"; $response =~ s/.....//;
+	# 1B Software Version
+	print "  software version: ".substr($response,0,1)."\n";
+	$response =~ s/.//;
+	# 2B data length
+	$dataLength = substr($response,0,2);
+	$response =~ s/..//;
+	print "  data length: 0x".$dataLength;
+	$dataLength = atoi($dataLength);
+	print " ($dataLength)\n";
+	# up to 256B data
+	if ( $dataLength < 9 ) { print "error: Data length less than minimum.\n" and exit; }
+	$yamaha{'System'} = substr($response,7,1);
+	$yamaha{'Power'} = substr($response,8,1);
+	if ( $yamaha{'System'} eq "0" )
+	{
+		if ( $yamaha{'Power'} eq "0" ) {
+			if ( $dataLength != 9 ) { print "error: Data length greater than expected.\n" and exit;
+			}
+		} else {
+			if ( $dataLength != 138 ) { print "error: Data length not as expected.\n" and exit;
+			}
+		}
+	}
+	print "  data: ";
+	while ( $dataLength > 0 )
+	{
+		print substr($response,0,1);
+		$response =~ s/.//;
+		$dataLength--;
+	}
+	print "\n";
+	# 2B checksum
+	$checksum = substr($response,0,2);
+	print "  checksum: 0x".$checksum;
+	$checksum = atoi($checksum);
+	print " ($checksum)\n";
+	$response =~ s/..//;
+	# ETX
+	$response =~ s/(.)//;
+	$_ = str2hex($1);
+	if ( m/03/ ) { print(" ETX\n"); }
+	else { print "error: No ETX\n" and exit; }
+	print "\n";
+}
+
+sub sendControl # getReport
+{
+	local($inStr) = @_;
+
+	print "Sending Control $inStr...\n";
+	print "\$yamaha{'ModelID'} $yamaha{'ModelID'}\n";
+	print "\$Spec{\$yamaha{'ModelID'}} $Spec{$yamaha{'ModelID'}}\n";
+	print "\$Spec{\$yamaha{'ModelID'}}{'Control'} $Spec{$yamaha{'ModelID'}}{'Control'}\n";
+	$source = $Spec{$yamaha{'ModelID'}}{'Control'};
+	$cdr = $inStr;
+	$packet = "";
+	$packetTail = "";
+	$count = 0;
+	while ( "" ne $cdr )
+	{
+		if ( $count > 10 ) { exit; }
+		$count++;
+		if ( defined($source{'Prefix'}) ) { $packet = $packet.$source{'Prefix'}; }
+		if ( defined($source{'Suffix'}) ) { $packetTail = $source{'Suffix'}.$packetTail; }
+		print "cdr $cdr\n";
+		$cdr =~ s/(\S+)\s*//;
+		$car = $1;
+		print "car $car cdr $cdr source source\n$packet $packetTail\n";
+		if ( "" ne $cdr )
+		{
+			print("source $source\n");
+			print("source keys");
+			for $key ( keys %$source ) { print " ".$key; }
+			print "\n";
+			print("source{$car} ".$source{$car}."\n");
+			$source = $source{$car};
+		} else {
+			print("source{$car} $source{$car}\n");
+			$packet = $packet.$source{$car}.$packetTail;
+		}
+	}
+	print "send $packet";
+	#print $yamaha{'ModelID'} $Spec{$yamaha{'ModelID'}}{'Configuration'}\n";
+	return;
+
+	# Rules
+	if ( $yamaha{'Power'} ne $Spec{$yamaha{'ModelID'}}{'Configuration'}{'Power'}{'ON'} )
+	{
+		#print "error: Only Power and System Control commands are valid when the power is off.\n" and exit;
+	}
+	if ( $yamaha{'System'} ne $Spec{$yamaha{'ModelID'}}{'Configuration'}{'System'}{'OK'} )
+	{
+		print "error: No Control commands are allowed when the system status is not OK.\n" and exit;
+	}
+
+	print "Sending Control $inStr...\n";
+	$PortObj->write($STX."07EB".$inStr.$ETX);
+	($count_in, $string_in) = $PortObj->read(275);
+	print "Got $count_in bytes back.\n";
+	$string_in = "";
+	print "\n";
+}
+sendInit();
+sendReady();
+#sendControl("B");
+#sleep 10;
+#sendControl("E");
+sendControl("Operation Power ON");
+undef $PortObj;
+exit;
 
 print "setting variables 0 and 1 to 1...\n";
 $varMSG = "FE	00	00	FF	1C E3 00 FF 02 FD 01 FE 00 FF 1F E0";
@@ -138,6 +331,22 @@ while ($client = $server->accept())
 #close the port - when the server is shut down
 undef $PortObj;
 
+
+sub atoi
+{
+	local($inStr) = @_;
+	$retVal = 0;
+	while ( $inStr =~ s/(.)// )
+	{
+		$char = $1;
+		$retVal <<= 4;
+		$num = ord($char);
+		if ( $num >= 0x30 && $num <= 0x39 ) { $retVal += ($num - 0x30); }
+		elsif ( $num >= 0x41 && $num <= 0x46 ) { $retVal += (10 + $num - 0x41); }
+		else { print "atoi error at $char ($num) $retVal $inStr\n" and exit; }
+	}
+	return $retVal;
+}
 
 sub getMsg 
 {
